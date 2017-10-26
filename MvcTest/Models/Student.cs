b52c8db130860781d544b470ed58bf971f755e4a
junwang <junwang@KNX-CD-DP-0015>
2017-10-26 17:26:33 +0800

    添加项目文件。

---
+++ MvcTest/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcTest.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+        public int Gender { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Major { get; set; }
+        public DateTime EntranceDate { get; set; }
+    }
+}
---
+++ MvcTest/Models/StudentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcTest.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext() : base("StudentDbContext")
+
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
