6fa265843684a0e1cb827f454203a77d7c6c6eea
junwang <junwang@KNX-CD-DP-0015>
2017-10-30 18:02:17 +0800

    11

--- MvcTest/Controllers/HomeController.cs
+++ MvcTest/Controllers/HomeController.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MvcTest.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string UserName, string Password)
+        {
+            if (UserName == "admin" && Password == "111111")
+            {
+                FormsAuthentication.RedirectFromLoginPage("admin", false);
+            }
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
         public ActionResult Index()
         {
             return View();
